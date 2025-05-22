29a9d279d6bd28a4937ecb2474dcc5aaadc69027
Julia Billing <juliabilling93@gmail.com>
2025-05-22 20:15:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/* Menyikon som knapp för öppning av overlay meny */
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
 document.addEventListener("DOMContentLoaded", function() {
     const carousel = document.querySelector(".carousel");
     const arrowBtns = document.querySelectorAll(".wrapper i");
